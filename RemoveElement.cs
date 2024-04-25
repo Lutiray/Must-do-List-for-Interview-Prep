d6e1b01a8d9fa3fa3d69463d22b65455a3b5f38c
Natalia Eremina <entalia663@gmail.com>
2024-04-25 22:56:45 +0200

    Description of task and solution was added

--- RemoveElement.cs
+++ RemoveElement.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+//Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the 
+//elements may be changed. Then return the number of elements in nums which are not equal to val.
 
-namespace TasksFromLeetcode
+//Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do 
+//the following things:
+
+//Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+//The remaining elements of nums are not important as well as the size of nums.
+//Return k.
+
+public class Solution1
 {
-    internal class RemoveElement
+    public int RemoveElement(int[] nums, int val)
     {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+
+        return count;
     }
 }
